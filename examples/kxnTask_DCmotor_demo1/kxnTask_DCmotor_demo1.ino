f6929659008f84df5fe205b78a7e0b5f2e78917a
MinhKhanh614 <0308231027@caothang.edu.vn>
2024-09-06 14:30:39 +0700

    update khanhDelay(unsigned long)

--- examples/kxnTask_DCmotor_demo1/kxnTask_DCmotor_demo1.ino
+++ examples/kxnTask_DCmotor_demo1/kxnTask_DCmotor_demo1.ino
@@ -7,20 +7,24 @@ long value1, value2, value3;
 
 void setup()
 {
-  motor.setup(1, 2, 3, 100);
+  Serial.begin(9600);
+  motor.setup(4, 7, 5, 100);
   // motor.write(-100,5000);
 }
 
 void loop()
 {
-  motor.write(30,3000);
+  Serial.println(millis());
+  // testSerial();
+  // motor.write(value1,value2);
   // kxnTaskManager.run(millis());
+  // delay(5000);
   khanhDelay(5000);
 }
 
 void khanhDelay(unsigned long Time)
 {
-  while (millis() <= millis() + Time)
+  while (millis() <= millis() + Time - 1000)
   {
     kxnTaskManager.run(millis());
     currentMillis = millis();
@@ -34,4 +38,12 @@ void khanhDelay(unsigned long Time)
         Time = 0;
     }
   }
+}
+void testSerial()
+{
+    if (Serial.available())
+    {
+        String inputText = Serial.readStringUntil('\n');
+        sscanf(inputText.c_str(), "%ld %ld %ld", &value1, &value2, &value3);
+    }
 }
